06ef16c27db3f2e8fe52bb6a7e6db85527af1257
Jason <jason@jasons-macbook-pro.local>
2017-10-24 23:38:42 -0400

    Added a button to the Main.Storyboard, aligned it using Auto Layout to the center of the screen. Added code in the ViewController that prompts the user to allow notifications when opening the app for the first time. Also added an action function for the button that pushes a notification when the user presses the button. The notification will not appear when the app is open, so there is 10 seconds from when the button is pressed to when the notification appears. Hit the button and close out of the app, the notification will appear in several seconds.
    The UNNotification framework used by iOS has a built in trigger function that allows a notifications to be pushed depending on location. The one used here is a time interval trigger function. The location one take lat and long as parameters, and also allows a circular region. This will be very helpful when setting up location notifications in the future.

--- LocationBasedSurveyApp/ViewController.swift
+++ LocationBasedSurveyApp/ViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import UserNotifications 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+    
+    var isGrantedNotificationAccess:Bool = false
+    
+    //MARK: Properties
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Prompts user to allow notifications when it first loads
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: [.alert,.sound,.badge],
+            completionHandler: { (granted,error) in
+                self.isGrantedNotificationAccess = granted
+            }
+        )
+        
+        // for remote access when we get that far
+        //application.registerForRemoteNotifications()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +36,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Actions
+    @IBAction func notificationButton(_ sender: UIButton) {
+        if isGrantedNotificationAccess{
+            let content = UNMutableNotificationContent()
+            content.title = "Button Notification Test"
+            content.subtitle = "Location Based Survey App"
+            content.body = "Notification Test Succeeded!!!"
+            
+            // Gives time to exit the app, notification will not appear if app is open
+            let trigger = UNTimeIntervalNotificationTrigger(
+                timeInterval: 10.0,
+                repeats: false)
+            
+            //Set the request for the notification from the above
+            let request = UNNotificationRequest(
+                identifier: "button.test",
+                content: content,
+                trigger: trigger
+            )
+            
+            //Add the notification to the currnet notification center
+            UNUserNotificationCenter.current().add(
+                request, withCompletionHandler: nil)
+            
+        }
+        
+        // print("Button test")
+        
+    }
+    
 }
 
